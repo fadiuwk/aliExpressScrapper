197ed2f8ca6782c6ebbdcbdaf601fee8e1910908
Fady Fares <fady.fares@hitop.com.sa>
2022-09-11 23:37:03 +0200

    update1-2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require("express");
 const puppeteer = require('puppeteer');
+const fs = require('fs/promises')
 const { nanoid } = require('nanoid');
 const port = process.env.PORT || 3000;
 
 // var userAgent = require("user-agents");
 
-const aliExpressScraper = require('./aliexpressProductScraper')
+const aliExpressScraper = require('./aliexpressProductScraper');
+const { writeFile } = require('fs');
 
 const app = express();
 
@@ -17,58 +19,69 @@ app.get('/', (req, res) => res.send("Hello World!"))
 app.post('/scrapingData', async (req, res) => {
     const { urls } = req.body;
 
-    let result = await scrapProduct(urls)
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    res.json({ message: "Done", result })
+    await page.goto("https://learnwebcode.github.io/practice-requests/")
+
+    const names = await page.evaluate(()=>{
+        return Array.from(document.querySelectorAll('.info strong')).map(x => x.textContent)
+    })
+
+    writeFile("names.txt" , names.join("\r\n"))
+
+    // let result = await scrapProduct(urls)
+
+    res.json({ message: "Done"})
 })
 
 
-const scrapProduct = async (urls) => {
+// const scrapProduct = async (urls) => {
 
-    let aliProducts = [];
-    const browser = await puppeteer.launch({
-        headless: true,
-        ignoreDefaultArgs: ['--disable-extensions']
-    });
-    const page = await browser.newPage();
+//     let aliProducts = [];
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         ignoreDefaultArgs: ['--disable-extensions']
+//     });
+//     const page = await browser.newPage();
 
-    try {
-        // await page.setUserAgent(userAgent.toString());
-        for (let i = 0; i < urls.length; i++) {
-            await page.goto(urls[i], {
-                waitUntil: 'networkidle2', timeout: 0
-            });
-            // const id = Number(urls[i].split('/')[4].split('.')[0]);
-            // const aliProduct = await aliExpressScraper(id);
-            // aliProduct.sku = nanoid();
-            // if (aliProduct.salePrice === undefined) {
-            //     aliProduct.salePrice = ''
-            // }
-            // aliProduct.productUrl = urls[i];
-
-
-            let [el] = await page.$x('//*[@class="product-dynamic-shipping"]/div/div/div/span/strong/span');
-            let txt = await el?.getProperty('textContent');
-            let shipping = await txt?.jsonValue()
-            let shippingPrice = Number(shipping?.toString().replace(/[^0-9.]/g, ''));
-            // aliProduct.shippingPrice = shippingPrice;
-
-            aliProducts.push(shippingPrice);
-        }
-
-        // console.log(aliProducts);
-        return aliProducts;
-
-    } catch (e) {
-        // res.send({ data: null });
-        console.log('something error , please try again...', e)
-
-    } finally {
-        await browser.close();
-    }
+//     try {
+//         // await page.setUserAgent(userAgent.toString());
+//         for (let i = 0; i < urls.length; i++) {
+//             await page.goto(urls[i], {
+//                 waitUntil: 'networkidle2', timeout: 0
+//             });
+//             // const id = Number(urls[i].split('/')[4].split('.')[0]);
+//             // const aliProduct = await aliExpressScraper(id);
+//             // aliProduct.sku = nanoid();
+//             // if (aliProduct.salePrice === undefined) {
+//             //     aliProduct.salePrice = ''
+//             // }
+//             // aliProduct.productUrl = urls[i];
 
 
-}
+//             let [el] = await page.$x('//*[@class="product-dynamic-shipping"]/div/div/div/span/strong/span');
+//             let txt = await el?.getProperty('textContent');
+//             let shipping = await txt?.jsonValue()
+//             let shippingPrice = Number(shipping?.toString().replace(/[^0-9.]/g, ''));
+//             // aliProduct.shippingPrice = shippingPrice;
+
+//             aliProducts.push(shippingPrice);
+//         }
+
+//         // console.log(aliProducts);
+//         return aliProducts;
+
+//     } catch (e) {
+//         // res.send({ data: null });
+//         console.log('something error , please try again...', e)
+
+//     } finally {
+//         await browser.close();
+//     }
+
+
+// }
 
 app.listen(port, () => {
     console.log("running.....");
