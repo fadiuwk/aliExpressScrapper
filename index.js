e24912fd8093671439bd6a065ad94a8ad740f4d6
Fady Fares <fady.fares@hitop.com.sa>
2022-09-12 00:55:34 +0200

    inshallah run

--- index.js
+++ index.js
@@ -14,26 +14,27 @@ const url = 'https://www.theguardian.com/us'
 
 app.get('/', (req, res) => res.send("Hello World!"))
 
-app.get('/home', async (req, res) => {
+app.get('/home', (req, res) => {
     axios(url)
-    .then(res => {
-        const html = res.html;
-        const $ = cheerio.load(html);
-        const articles = []
-        $('.fc-item__title' , html).each(function (){
-            const title = $(this).text();
-            const url = $(this).find('a').attr('href');
-            articles.push({
-                title , 
-                url
+        .then(response => {
+            const html = response.html;
+            const $ = cheerio.load(html);
+            const articles = []
+            $('.fc-item__title', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).find('a').attr('href');
+                articles.push({
+                    title,
+                    url
+                })
+
             })
-            
-        })
-        // console.log({articles});
-        res.json({articles})
-    }).catch(error => console.log(error))
+            // console.log({articles});
+            res.json({articles})
+            // res.send()
+        }).catch(error => console.log(error))
 })
 
 
 
-app.listen(port, () =>  console.log(`server running on port.....${port}`))
+app.listen(port, () => console.log(`server running on port.....${port}`))
