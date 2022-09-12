a0ee7d87919d2f01c2d5e10ee97d030a5f7d4414
Fady Fares <fady.fares@hitop.com.sa>
2022-09-12 11:41:54 +0200

    chroinum fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/scrapingData', async (req, res) => {
     axios(url)
         .then(async (response) => {
             let aliProducts = [];
-            const browser = await puppeteer.launch({headless: false});
+            const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox']});
             const page = await browser.newPage();
         
             try {
